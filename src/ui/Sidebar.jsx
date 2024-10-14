ad84eea1934fe1a7e85cb6727eab5b52ed129ffe
Mohammad Armaan <mohammadarmaan7786@Mohammads-MacBook-Air.local>
2024-10-14 20:12:28 +0530

    Altered styles

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { HiX } from "react-icons/hi";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
-import Uploader from '../data/Uploader'
+// import Uploader from '../data/Uploader'
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0); 
@@ -53,7 +53,7 @@ function Sidebar({ toggleSidebar }) {
       </CloseButton>
       <Logo />
       <MainNav toggleSidebar={toggleSidebar} />
-      <Uploader />
+      {/* <Uploader /> */}
     </StyledSidebar>
   );
 }
